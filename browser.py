2a94036e32db60e427824c53c3f88fae499f0354
simon geletaw <simongeletaw2@gmail.com>
2025-07-25 17:16:03 -0400

    draft

--- browser.py
+++ browser.py
@@ -1,18 +1,11 @@
 import socket
-
-# Create a socket and connect to the server
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('example.com', 80))  # Using HTTP (port 80)
-
-# Send a properly formatted HTTP GET request
-cmd = 'GET / HTTP/1.1\r\nHost: example.com\r\n\r\n'.encode()  # Correct format
+mysock.connect(('example.com', 80))  
+cmd = 'GET / HTTP/1.1\r\nHost: example.com\r\n\r\n'.encode()  
 mysock.send(cmd)
-
-# Receive and print the response
 while True:
-    data = mysock.recv(512)  # Read 512 bytes at a time
+    data = mysock.recv(512)  
     if len(data) < 1:
         break
     print(data.decode(), end='')
-
-mysock.close()  # Close the connection
+mysock.close()  
